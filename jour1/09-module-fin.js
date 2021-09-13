a01c0975c55759a51ed5ebc0aa41b97827c35a92
Malik H <malik.h@webdevpro.net>
2021-09-13 12:37:56 +0200

    enoncé

--- jour1/09-module-fin.js
+++ jour1/09-module-fin.js
@@ -11,3 +11,24 @@ function serveur(){
 module.exports = serveur  ;
 
 // 09-main.js
+
+
+// créer le fichier 10-exo.js 
+// ce fichier contient une variable :
+
+/*
+vehicule qui est objet littéral 
+    propriete : 
+        anneeAchat 2019
+        proprietaire Alain
+    méthode
+        achat : le nombre d'année entre de jours entre aujoud'hui et l'année d'achat
+
+exporter cet objet littéral 
+
+créer le fichier 10-main.js 
+lors de l'import exécuter la méthode achat de l'objet litteral et l'exécuté directement
+et l'afficher dans la console
+
+*/ 
+
