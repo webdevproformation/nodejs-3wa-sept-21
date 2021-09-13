2bc2e047c1eccdde877934195f9a817fd868c0ce
Malik H <malik.h@webdevpro.net>
2021-09-13 11:41:17 +0200

    exo 7

--- jour1/06-module-suite.js
+++ jour1/06-module-suite.js
@@ -15,3 +15,22 @@ etudiant.description();
 // message d'erreur ReferenceError: distance is not defined
 // stack d'exécution : la pile d'exécution 
 // l'ensemble des fichiers / des fonctions qui ont été exécuté par nodejs pour exécuter votre fichier .js 
+
+/*
+cas pratique : 
+
+
+    1 Créer un fichier 07-librairie.js contenant deux fonctions
+    une fonction addition avec deux arguments a et b retournant la somme de a plus b
+    une fonction multiplication avec deux arguments a et b retournant la multiplication de a plus b
+    Ces deux fonctions doivent être exportées
+
+    2 Créer un fichier 07-calcul.js important les deux fonctions de 07-librairie.js
+    Réaliser les calculs suivants en utilisant les fonctions et les afficher dans la console:
+
+    2 + 53
+    12,4 x 33
+    -2 x 24,65
+
+
+*/ 
