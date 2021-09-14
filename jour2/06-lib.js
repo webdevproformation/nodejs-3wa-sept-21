19daeee4321482741fa5a8cd01e4462443ce5bd7
Malik H <malik.h@webdevpro.net>
2021-09-14 14:35:01 +0200

    cas pratique 07

--- jour2/06-lib.js
+++ jour2/06-lib.js
@@ -20,3 +20,23 @@ function empty(variable){
 
 module.exports = empty ;
 // 06-lib.test.js
+
+// créer un nouveau fichier 07-exo.js
+// cet fichier contient une fonction qui permet de palindrome 
+// mot qui peuvent se lire dans les deux sens 
+// lol 
+// kayak 
+// Laval
+// répondre true 
+
+// si vous lui donnez comme valeur 
+// bonjour
+// hello
+// salut 
+// répondre false 
+
+// créer la fonction palindrome 
+// créer un fichier 07-exo.test.js qui vérifie chacun des cas mentionnés via la librairie jest
+
+
+
